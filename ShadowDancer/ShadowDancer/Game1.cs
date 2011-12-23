c5ccc29006912773281d97123699fd36fbaaf5af
RandomOutput <dpx109@gmail.com>
2011-12-22 23:07:15 -0800

    tracking kinect data and adding to engine.

--- ShadowDancer/ShadowDancer/Game1.cs
+++ ShadowDancer/ShadowDancer/Game1.cs
@@ -17,6 +17,7 @@ using FarseerPhysics.Controllers;
 using FarseerPhysics.Collision;
 using System.Diagnostics;
 using FarseerPhysics.DebugViews;
+using Microsoft.Research.Kinect.Nui;
 
 namespace ShadowDancer
 {
@@ -35,9 +36,16 @@ namespace ShadowDancer
         Body myBody;
         Body compund;
 
-        private uint[] data;
-        private Vertices verts;
-        private Vector2 scale;
+        //Kinect
+        Runtime nui;
+        //kinect data
+        Texture2D kinectDepth;
+
+        //float i = 0f;
+
+        //private uint[] data;
+        //private Vertices verts;
+        //private Vector2 scale;
 
         Texture2D polyonTexture;
         Texture2D circleTexture;
@@ -47,6 +55,8 @@ namespace ShadowDancer
         public DebugViewXNA debugViewXNA;
 
         private const float MeterInPixels = 64f;
+
+        Boolean _player = false;
 
         public Game1()
         {
@@ -62,15 +72,64 @@ namespace ShadowDancer
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            world = new World(new Vector2(0,5));
+            // Create the world for Farseer
+            world = new World(new Vector2(0,0.01f));
+
+            //set the window size [800x600]
             graphics.PreferredBackBufferHeight = 600;
             graphics.PreferredBackBufferWidth = 800;
-            debugViewXNA = new DebugViewXNA(world);
+
+            //initialize the kinect
+            nui = Runtime.Kinects[0];
+            nui.Initialize(RuntimeOptions.UseDepthAndPlayerIndex);
+
+            nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex);
+            nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
 
             base.Initialize();
         }
-    
+
+        private void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e) {
+            PlanarImage p = e.ImageFrame.Image;
+            _player = false;
+            Color[] DepthColor = new Color[p.Height * p.Width];
+
+            float maxDist = 4000;
+            float minDist = 850;
+            float distOffset = maxDist - minDist;
+
+            kinectDepth = new Texture2D(GraphicsDevice, p.Width, p.Height);
+            
+            int index = 0;
+
+            for (int y = 0; y < p.Height; y++)
+            {
+                for (int x = 0; x < p.Width; x++, index += 2)
+                {
+                    int n = (y * p.Width + x) * 2;
+                    int distance = (p.Bits[n + 0] | p.Bits[n + 1] << 8);
+                    int player = distance % 8;
+                    distance = distance / 8;
+                    if (player != 0)
+                    {
+                        if (!_player)
+                        {
+                            _player = true;
+                        }
+                        byte intensity = (byte)(255 - (255 * Math.Max(distance - minDist, 0) / (distOffset)));
+                        //DepthColor[y * p.Width + x] = new Color(intensity, intensity, intensity);
+                        DepthColor[y * p.Width + x] = new Color(255, 255, 255, 255);
+                    }
+                    else
+                    {
+                        DepthColor[y * p.Width + x] = new Color(0, 0, 0, 0);
+                    }
+                }
+            }
+
+            kinectDepth.SetData(DepthColor);
+        }
+
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
@@ -83,34 +142,29 @@ namespace ShadowDancer
             polyonTexture = Content.Load<Texture2D>("colMask");
             circleTexture = Content.Load<Texture2D>("circ");
             
-            data = new uint[polyonTexture.Width * polyonTexture.Height];
-            polyonTexture.GetData(data);
-            verts = PolygonTools.CreatePolygon(data, polyonTexture.Width, false);
-            scale = new Vector2(0.015f, -0.015f);
-            verts.Scale(ref scale);
-            
-            List<Vertices> _list = BayazitDecomposer.ConvexPartition(verts);
+            /*
+            data = new uint[kinectDepth.Width * kinectDepth.Height];
+            kinectDepth.GetData(data);
+            if (kinectDepth != null)
+            {
+                verts = PolygonTools.CreatePolygon(data, kinectDepth.Width, false);
+                scale = new Vector2(0.015f, -0.015f);
+                verts.Scale(ref scale);
 
+                List<Vertices> _list = BayazitDecomposer.ConvexPartition(verts);
+            }
+            */
             // TODO: use this.Content to load your game content here
             
 
-            Vector2 circPos = new Vector2(500, 100) / MeterInPixels;
+            Vector2 circPos = new Vector2(500, 105) / MeterInPixels;
             myBody = BodyFactory.CreateCircle(world, (32f / MeterInPixels)/2, 1, circPos);
             myBody.BodyType = BodyType.Dynamic;
-            myBody.Restitution = .2f;
-            myBody.Friction = 0.5f;
-
-            /*
-            Vector2 groundPos = new Vector2(510, 200) / MeterInPixels;
-            //compund = BodyFactory.CreateCircle(world, (32f / MeterInPixels) / 2, 1, groundPos);         
-            compund = BodyFactory.CreateCompoundPolygon(world, _list, 1f, groundPos);
-            compund.BodyType = BodyType.Static;
-            */
-            
-            
+            myBody.Restitution = .4f;
+            myBody.Friction = 0.5f;        
             
 
-            debugViewXNA.LoadContent(GraphicsDevice, Content);
+            //debugViewXNA.LoadContent(GraphicsDevice, Content);
         }
 
         /// <summary>
@@ -138,15 +192,26 @@ namespace ShadowDancer
                 world.RemoveBody(compund);
             }
             
+            //define the origin, I probably don't need the defenition here as it exists.
             gOrigin = new Vector2(
                 polyonTexture.Width / 2f,
                 polyonTexture.Height / 2f);
-            List<Vertices> vertices = ToVertices(polyonTexture, out gOrigin);
-            compund = BodyFactory.CreateCompoundPolygon(world, vertices, 1f);
-            compund.Position = new Vector2(510, 300) / MeterInPixels;
-            // TODO: Add your update logic here
-            world.Step((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f);
 
+            //uses the toVerticies code (slightly modified) found at: http://farseerphysics.codeplex.com/discussions/246636
+            if (_player)
+            {
+                List<Vertices> vertices = ToVertices(kinectDepth, out gOrigin);
+
+                //creates a body based on the verticies in the List verticies
+                compund = BodyFactory.CreateCompoundPolygon(world, vertices, 1f);
+
+
+                //sets the position of the ground sprite and converts pixels to meters for the engine.
+                compund.Position = new Vector2(0, 0) / MeterInPixels;
+            }
+            // Steps the game forward.
+            world.Step((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f);
+            //i -= .1f;
             base.Update(gameTime);
         }
 
@@ -158,9 +223,12 @@ namespace ShadowDancer
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+
+            /* This takes the positions for the circle and ground positions 
+             * from the engine in meters and converts them to pixels.
+             */
             Vector2 circlePos = myBody.Position * MeterInPixels;
-            Vector2 groundPos = compund.Position * MeterInPixels;
+            //Vector2 groundPos = compund.Position * MeterInPixels;
 
             Vector2 circleOrigin = new Vector2(
                 circleTexture.Width/2f,
@@ -170,29 +238,17 @@ namespace ShadowDancer
                 polyonTexture.Height/2f);
 
             spriteBatch.Begin();
+            //draw the kinect data
+            if (_player)
+            {
+                spriteBatch.Draw(kinectDepth, new Rectangle(0, 0, 800, 600), Color.White);
+            }
+            //draw game data
             spriteBatch.Draw(circleTexture, circlePos, null, Color.White, 0f, circleOrigin, 1f, SpriteEffects.None, 0f);  
-            spriteBatch.Draw(polyonTexture, groundPos, null, Color.White, 0f, gOrigin, 1f, SpriteEffects.None, 0f);
-            DebugDraw();
+            //spriteBatch.Draw(polyonTexture, groundPos, null, Color.White, 0f, gOrigin, 1f, SpriteEffects.None, 0f);
             spriteBatch.End();
 
             base.Draw(gameTime);
-        }
-
-        protected void DebugDraw()
-        {
-            Matrix proj = Matrix.CreateOrthographic(
-                graphics.PreferredBackBufferWidth / 1f / 100.0f,
-                -graphics.PreferredBackBufferHeight / 1f / 100.0f, 0, 1000000);
-            Vector3 campos = new Vector3();
-            campos.X = (graphics.PreferredBackBufferWidth / 2) / 100.0f;
-            campos.Y = (graphics.PreferredBackBufferHeight / 2) / -100.0f;
-            campos.Z = 0;
-            Matrix tran = Matrix.Identity;
-            tran.Translation = campos;
-            Matrix view = tran;
-
-            debugViewXNA.RenderDebugData(ref proj, ref view);
-            return;
         }
 
         static List<Vertices> ToVertices(Texture2D texture, out Vector2 origin)
@@ -220,21 +276,29 @@ namespace ShadowDancer
 
             //Since it is a concave polygon, we need to partition it into several smaller convex polygons
             //List<Vertices> list = BayazitDecomposer.ConvexPartition(textureVertices);
-            List<Vertices> list = EarclipDecomposer.ConvexPartition(textureVertices);
-
-            //Now we need to scale the vertices (result is in pixels, we use meters)
-            //At the same time we flip the y-axis.
-            var scale = new Vector2(0.015f, 0.015f);
-
-            foreach (Vertices vertices in list)
+            try
             {
-                vertices.Scale(ref scale);
+                List<Vertices> list = BayazitDecomposer.ConvexPartition(textureVertices);
+            
 
-                //When we flip the y-axis, the orientation can change.
-                //We need to remember that FPE works with CCW polygons only.
-                vertices.ForceCounterClockWise();
+                //Now we need to scale the vertices (result is in pixels, we use meters)
+                //At the same time we flip the y-axis.
+                var scale = new Vector2(0.015f, 0.015f);
+
+                foreach (Vertices vertices in list)
+                {
+                    vertices.Scale(ref scale);
+
+                    //When we flip the y-axis, the orientation can change.
+                    //We need to remember that FPE works with CCW polygons only.
+                    vertices.ForceCounterClockWise();
+                }
+                return list;
             }
-            return list;
+            catch
+            {
+                return new List<Vertices>();
+            }
         }
     }
 }
